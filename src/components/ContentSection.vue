5c5cd7119b091d65235cb70e4ba3e5653c1a76fb
tiger-githubb <aristidetiger12@gmail.com>
2022-07-14 20:23:49 +0000

    mise a jour

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -62,7 +62,7 @@
 
           <div class="row featurette">
             <div class="col-md-7 order-md-2">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing sed do eiusmod tempor
                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
                 nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br />
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
